2a8c1087c2a3ff5dc0e7f1c1c589beb27e255100
sallm.in <sallminrexha@gmail.com>
2024-12-01 00:03:28 +0100

    wip

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,7 @@ export default {
         './resources/**/*.blade.php',
         './resources/**/*.js',
         './resources/**/*.vue',
+        './vendor/filament/**/*.blade.php',
     ],
     theme: {
         extend: {
